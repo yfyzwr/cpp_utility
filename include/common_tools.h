039d5477c9e47a64ef7264868635a71972d8ecd3
yfyzwr <yfyzwr@163.com>
2017-06-19 17:44:22 +0800

    protoc makefile

---
+++ include/common_tools.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_TOOLS_H
+#define COMMON_TOOLS_H
+
+class common_tools{
+
+    class error_no{
+        static const int SUCCESS = 0;
+
+    };
+
+    class string_extend{
+
+    public:
+
+    };
+
+    class character_coding{
+
+    public:
+
+    };
+
+    class security_tool{
+
+    public:
+
+    };
+};
+
+#endif //COMMON_TOOLS_H
